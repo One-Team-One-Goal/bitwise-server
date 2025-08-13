1a96ed2d0ddbb50e00b18ebe8af47120bd3c56f3
valceven <valmykelcevenbolante@gmail.com>
2025-08-14 00:08:46 +0800

    added env + frontend cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,49 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  )
   await app.listen(process.env.PORT ?? 3000);
+  
+  const config = new DocumentBuilder()
+    .setTitle('Bitwise Server V2')
+    .setDescription('Bitwise Server API documentation for MARS BENITEZ')
+    .setVersion('2.0')
+    .addTag('bitwise')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('bitwise-api', app, documentFactory);
+
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors({
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        process.env.FRONTEND_URL,
+        process.env.LOCALHOST_URL,
+      ];
+
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Cors not allowed for this origin'), false);
+      }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'];
+  });
+
+  await app.listen(process.env.PORT || 3001, '0.0.0.0');
 }
+
 bootstrap();
