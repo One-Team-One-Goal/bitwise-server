cecf19d563eb71e916a00a7dd03a5a424844ecb6
valceven <valmykelcevenbolante@gmail.com>
2025-11-06 01:36:54 +0800

    build fix part 2

--- src/user-progress/user-progress.module.ts
+++ src/user-progress/user-progress.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserProgressController } from './user-progress.controller';
 import { UserProgressService } from './user-progress.service';
 import { PrismaModule } from 'prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [UserProgressController],
   providers: [UserProgressService],
   exports: [UserProgressService]
